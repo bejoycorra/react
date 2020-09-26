2ce11a9fcb80f87a3e07dd2ddf0d802ada24f128
bejoycorra <bejoy@corra.com>
2020-09-26 23:43:12 +0530

    return

--- MyReturns/ReturnsDetailsPage/ReturnComments.js
+++ MyReturns/ReturnsDetailsPage/ReturnComments.js
@@ -1,16 +1,20 @@
-import React, { Fragment, useMemo, useEffect } from 'react';
+import React, {Fragment, useMemo, useEffect, useState} from 'react';
 import { useTranslation } from 'react-i18next';
 import {combine, Field, isRequired, TextInput} from "@corratech/form-components";
 import {Button} from "react-bootstrap";
+import {useMutation} from "react-apollo";
+import submitReturnComment from "../Queries/submitReturnComment.graphql"
+import PropTypes from "prop-types";
 
 export const ReturnComments = props => {
     const { itemData, returnCommentsTitle } = props;
     const [t] = useTranslation();
-    const { comments } = itemData;
-debugger
+    const [returncomments, setReturncomments] = useState(itemData.comments);
+    const [comment, setComment] = useState('');
+
     const getReturnComments = useMemo(
         () =>
-            comments.map((comment, key) => (
+            returncomments.map((comment, key) => (
                 <div key={key}>
                     <div className="col comment">
                         {comment.is_admin 
@@ -22,20 +26,56 @@ debugger
                     </div>
                 </div>
             )),
-        [comments]
+        [returncomments]
     );
+    const [saveComment] = useMutation(
+        submitReturnComment,
+          {
+            onCompleted: res => {
+                debugger
+                console.log('comment'+res.addRmaComment.comments);
+                setReturncomments(res.addRmaComment.comments);
+            }
+          }
+        );
+    const saveReturnComment = async (comment) => {
+        await saveComment({
+            variables: {
+                id: itemData.entity_id,
+                comment
+            }
+        });
+    };
+    const submitForm = event => {
+        event.preventDefault();
+        if(comment !==''){
+            saveReturnComment(comment);
+        }
 
+    };
 const submitReturnComments = (
-    <form>
+    <form onSubmit={submitForm}>
         <Field
-            label={t('Leave comment')}
-            labelText={'comment'}
+            label={t(props.commentFieldLabel)}
+            labelText={props.commentFieldLabelText}
         >
             <TextInput
                 type={'text'}
+                autoFocus
                 field="comment"
                 id="comment"
+                validate={combine([
+                    {
+                        fn: isRequired,
+                        text: t(props.requiredText)
+                    }
+                ])}
+                placeholder={t(props.commentFieldPlaceholder)}
+                initialValue={''}
                 validateOnBlur
+                onChange={event =>
+                    setComment(event.target.value)
+                }
             />
         </Field>
         <Button
@@ -43,7 +83,7 @@ const submitReturnComments = (
             variant="primary"
             type="submit"
         >
-            {t('Submit Comment')}
+            {t(props.submitButtonText)}
         </Button>
     </form>
 );
@@ -51,9 +91,25 @@ const submitReturnComments = (
         <div className="return-comments-container">
             <div>{t(returnCommentsTitle)}</div>
             <div className="return-comments">
-                <div>{comments && getReturnComments}</div>
+                <div>{returncomments && getReturnComments}</div>
                 <div>{submitReturnComments}</div>
             </div>
         </div>
     );
+};
+
+ReturnComments.propTypes = {
+    requiredText: PropTypes.string,
+    submitButtonText: PropTypes.string,
+    commentFieldPlaceholder: PropTypes.string,
+    commentFieldLabelText: PropTypes.string,
+    commentFieldLabel: PropTypes.string
+};
+
+ReturnComments.defaultProps = {
+    requiredText: 'This is a required field',
+    submitButtonText: 'Submit Comment',
+    commentFieldPlaceholder: 'Return Comment',
+    commentFieldLabelText: 'comment',
+    commentFieldLabel: 'Leave comment',
 };
