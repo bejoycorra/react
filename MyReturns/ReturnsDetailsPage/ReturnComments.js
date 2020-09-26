475b00ba6aef0b5f16c2d4b92cf18c780113c419
bejoycorra <bejoy@corra.com>
2020-09-26 12:58:15 +0530

    sumit coemmt

--- MyReturns/ReturnsDetailsPage/ReturnComments.js
+++ MyReturns/ReturnsDetailsPage/ReturnComments.js
@@ -1,18 +1,23 @@
-import React, { Fragment, useMemo } from 'react';
+import React, { Fragment, useMemo, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import {combine, Field, isRequired, TextInput} from "@corratech/form-components";
+import {Button} from "react-bootstrap";
 
 export const ReturnComments = props => {
     const { itemData, returnCommentsTitle } = props;
     const [t] = useTranslation();
     const { comments } = itemData;
 debugger
-    const getReturnItems = useMemo(
+    const getReturnComments = useMemo(
         () =>
             comments.map((comment, key) => (
                 <div key={key}>
                     <div className="col comment">
-                        {comment.is_admin && t('Customer Service')}
-                        {comment.is_admin == 0 && t('customer')} {comment.created_at}
+                        {comment.is_admin 
+                            ? t('Customer Service ')
+                            : t('customer name ')
+                        }
+                        {comment.created_at}
                      <div>   {comment.comment} </div>
                     </div>
                 </div>
@@ -20,11 +25,34 @@ debugger
         [comments]
     );
 
+const submitReturnComments = (
+    <form>
+        <Field
+            label={t('Leave comment')}
+            labelText={'comment'}
+        >
+            <TextInput
+                type={'text'}
+                field="comment"
+                id="comment"
+                validateOnBlur
+            />
+        </Field>
+        <Button
+            size="lg"
+            variant="primary"
+            type="submit"
+        >
+            {t('Submit Comment')}
+        </Button>
+    </form>
+);
     return (
         <div className="return-comments-container">
             <div>{t(returnCommentsTitle)}</div>
             <div className="return-comments">
-                <div>{comments && getReturnItems}</div>
+                <div>{comments && getReturnComments}</div>
+                <div>{submitReturnComments}</div>
             </div>
         </div>
     );
